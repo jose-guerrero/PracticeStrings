662cd73bc0d0063ea5c7f7236dfb87129bb24308
jose-guerrero <jose.guerrerog@outlook.com>
2018-04-03 01:21:49 +1000

    adding custom exception

---
+++ ParseStringLibrary.MsTest/TestingParsing.cs
@@ -0,0 +1,50 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ParseStringLibrary.MsTest
+{
+    [TestClass]
+    public class TestingParsing
+    {
+        [TestMethod]
+        public void SimpleTest(){
+            string[] s1 = {"X","Y","Z"};
+            string[] s2 = {"1","2","3"};
+            var string1 = new ParseStrings("no{X}and{Y}and{Z}we",s1,s2);
+            Assert.IsTrue("no1and2and3we"==string1.res);
+        }
+
+        [TestMethod]
+        public void NullSimpleTest(){
+            string[] s1 = {"X","Y","Z"};
+            string[] s2 = {"0",null,"3"};
+            var string1 = new ParseStrings("no{X}and{Y}and{X}we",s1,s2);
+            Assert.IsTrue("no0andand0we"==string1.res);
+        }
+
+        [TestMethod]
+        public void OddNumberBrackets(){
+            string[] s1 = {"X","Y","Z"};
+            string[] s2 = {"1","2","3"};
+            var string1 = new ParseStrings("no{X}and{Yand{X}we",s1,s2);
+            Assert.IsTrue(""==string1.res);
+        }
+
+        [TestMethod]
+        public void NullInputs(){
+            string[] s1 = {"X","Y","Z"};
+            string[] s2 = {"1","2","3"};
+            var string1 = new ParseStrings("no{X}and{Yand{X}we",null,null);
+            Assert.IsTrue(false==string1.valid);
+        }
+
+        
+        [TestMethod]
+        public void CustomException(){
+            string[] s1 = {"X","Y","Z","A"};
+            string[] s2 = {"1","2"};
+            var string1 = new ParseStrings("no{X}and{Y}awe",s1,s2);
+            Assert.IsTrue(""==string1.res);
+        }
+    }
+}
---
+++ ParseStringLibrary/ParseStrings.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParseStringLibrary
+{
+    public class ParseStrings
+    {
+        public string res;
+        public bool valid;
+        public ParseStrings(string s,string[] X,string[] Y)
+        {
+            try
+            {
+                 
+                if (X.Length != Y.Length)
+                {
+                    throw new DifferentLengthException();  /// Custom exception
+                }
+
+                Dictionary<string, string> dictionary = new Dictionary<string, string>(); 
+                for (int i = 0; i < X.Length; i++)
+                {
+                    dictionary.Add(X[i],Y[i]);  
+                }
+
+                res = "";
+                while (true)
+                {
+                    int n = s.IndexOf('{');   /// Searching for '{'
+                    if (n==-1) break;         /// If it is not found then theres no more strings to change
+                    int m = s.IndexOf('}');
+
+                    string s1 = s.Substring(0,n);   
+                    res = res + s1 + dictionary[s.Substring(n+1,m-n-1)];
+                    s = s.Substring(m+1,s.Length-m-1);  /// Cutting to analyse the rest of the string
+                }
+                res += s;
+            }
+            catch
+            {
+                valid = false;
+                res = "";
+            }
+            
+        }       
+    }
+
+    [Serializable]
+    public class DifferentLengthException : Exception
+    {
+        public DifferentLengthException()
+        {         
+        }
+    }
+}
